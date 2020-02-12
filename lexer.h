f43774490465f1f4226d522cef5de00dddaa7dc4
sc18jt <sc18jt@leeds.ac.uk>
2020-02-12 21:16:15 +0000

    Base files added.

---
+++ lexer.h
@@ -0,0 +1,14 @@
+//
+// Created by sc18jt on 12/02/2020.
+//
+
+#ifndef COMP2932_LEXER_H
+#define COMP2932_LEXER_H
+
+
+class lexer {
+
+};
+
+
+#endif //COMP2932_LEXER_H
---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sc18jt on 12/02/2020.
+//
+
+#include "main.h"
